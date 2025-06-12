6c58cbc0327e65148c98fa5c337bf5722401530a
czh01 <2680728792@qq.com>
2025-06-12 19:16:57 +0800

    #

--- agent/main.go
+++ agent/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	cf "agent/config"
 	"agent/data"
+	"path/filepath"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/go-co-op/gocron"
@@ -19,7 +21,22 @@ func main() {
 	// 解析命令行参数
 	flag.Parse()
 	fmt.Printf("hostname:%s,token:%s\n", *hostName, *token)
-	config, err := cf.LoadConfig("./config/config.yaml")
+
+	// 获取当前可执行文件所在目录
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatalf("无法获取可执行文件路径: %v", err)
+	}
+	rootDir := filepath.Dir(exePath) // 获取可执行文件所在目录
+
+	// 构建 config.yaml 的绝对路径
+	configPath := filepath.Join(rootDir, "config", "config.yaml")
+	if err != nil {
+		log.Fatalf("failed to open config file:%v", err)
+		return
+	}
+
+	config, err := cf.LoadConfig(configPath)
 	if err != nil {
 		log.Fatalf("failed to open config file:%v", err)
 		return
